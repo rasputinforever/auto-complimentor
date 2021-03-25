9b46266b057fe2bc4c3e3b616c20ed1b20ca657f
rasputinforever <erik.justin.portillo@gmail.com>
2021-03-25 12:37:09 -0700

    basic outline

---
+++ auto-complimentor/db/mentors.js
@@ -0,0 +1,82 @@
+// this is the pseudo-db as I do not know best practices for database connection to React yet!
+
+const Mentors = [
+    {
+        name: 'Susan',
+        font: 'Arial',
+        image: 'susan.png',
+        keyWords: ['motherly', 'coddling', 'calm', 'guilt'],
+        messages: [
+            'In my eyes you are perfect in every way. You make me so proud; you give my life so much purpose, thank you!',
+            'I could never imagine something like this for you, you have always been the light of my life.' ,
+            'You? A problem? All this time you have always risen up to your challenges in your own special way.',
+            'Call it what you will but I think this problem of yours is actually a blessing in disguise, just as you are a blessing to me.',
+            'I can\'t bear to see you troubled, it makes me so proud to be part of your life when you rise to these challenges.',
+        ]
+    },
+    {
+        name: 'Dirk',
+        font: 'Arial',
+        image: 'dirk.png',
+        keyWords: ['fatherly', 'distracted', 'glory-days', 'boostraps'],
+        messages: [
+            'You know what I think, Sport? I think you need to toughen up a bit. Get some grit, and tackle this problem head on!',
+            'Back in my day we did\'t have problems. We faced every challenge with gusto. Like the time me and the boys won the regionals. Those were the days...' ,
+            'Sport, I just don\'t see what the problem is. If it were me in your shoes the first thing I\'d do is pull up those bootstraps, grin, and bare it!',
+            'Why is your generation so concerned with these problems? I don\'t know if I\'ll ever understand, life isn\'t so complicated if you toughen up!',
+            'Back in my day, Sport, we just called a spade a spade and that was that.',
+        ]
+    },
+    {
+        name: 'Spot',
+        font: 'Arial',
+        image: 'spot.png',
+        keyWords: ['dog'],
+        messages: [
+            'Bark! Bark bark bark!',
+            'Bark! ... Bark! ... Bark bark!!' ,
+            '... (Spot is sleeping)',
+            'Grrr... Bark!',
+            '... Woof.',
+        ]
+    },
+    {
+        name: 'Paul',
+        font: 'Arial',
+        image: 'paul.png',
+        keyWords: ['unrelated', 'specific', 'analytical'],
+        messages: [
+            'In my estimation your problem is as follows: you haven\'t started saving for retirement yet. Though it\'s never too late, at this point in the game there aren\'t a lot of minutes left in the quarter, so to speak.',
+            'What I\'m seeing here is basic statistics. You\'re the outlier here and the standard deviation is narrow enough that by your point of view you\'re basically in outer space!' ,
+            'This really reminds me of the American War of 1812. Oft forgotten, but really wild when you think about how it was basically an epilogue to the American Revolution and that we got our Anthem at that time.',
+            'Your problem is like the Quadratic Equation. It seems intimidating, especially when you\'re in high school, but it\'s actually quite beautiful when you revisit it as an older, wiser version of yourself.',
+            'My analytical self says that your problem is actually not a problem at all. Just think about it like that and you\'ll be good to go!',
+        ]
+    },
+    {
+        name: 'Cindy',
+        font: 'Arial',
+        image: 'cindy.png',
+        keyWords: ['waitress', 'diner', 'hurried'],
+        messages: [
+            'Hon, I got pork chops and the rib eye on special; sorry, not sure if that answered your question about our specials.',
+            'Here is your water, by the way, I\'m taking a 15 minute break so Jose over there is going to take your food order.' ,
+            'Did you want a slice of pie? Just the check? Ok, great.',
+            'I actually really like the liver and onions dinner, call me crazy, but it\'s really surprisingly good if you give it a chance.',
+            'Is Diet Pepsi OK?',
+        ]
+    },
+    {
+        name: 'Plork',
+        font: 'Arial',
+        image: 'plork.png',
+        keyWords: ['alien', 'nervous', 'hurried'],
+        messages: [
+            'Haha, yes, I can relate completely to your problem! I have also experienced that problem many times. Thank you!',
+            'When I was a human child I had this problem occur to me many times and on each time it occured to me I also had trouble with that problem!',
+            'It would seem for us, normal human people, that we are often challenged by problems such as this one.',
+            'When I arrived to this land mass and began interacting with its inhabitants I was struck by both how sentient the beings here are as well as how troubled they appear to be.',
+            'This reminds me of a funny annecdote that happened to me; I was imbibing on the typical meal of this region that we all enjoy and noticed a small creature running about on the ground of that establishment. I was not too sure if it was also a sentient create, such as ourselves, but I was too nervous to ask it.',
+        ]
+    },
+]
---
+++ auto-complimentor/src/components/Counter.js
@@ -0,0 +1,45 @@
+import React from "react";
+
+class Counter extends React.Component {
+  state = {
+    count: 0
+  };
+
+  handleIncrement = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  handleDecrement = () => {
+    this.setState({ count: this.state.count - 1 });
+  };
+
+  
+
+
+
+  render() {
+    
+    const cardBody = (
+      <div className="card-body">
+        <p className="card-text">Click Count: {this.state.count}</p>
+        <button className="btn btn-primary" onClick={this.handleIncrement}>
+          Increment
+        </button>
+        <button className="btn btn-primary" onClick={this.handleDecrement}>
+          Increment
+        </button>
+      </div>
+    )
+
+    return (
+      <div className="card text-center">
+        <div className="card-header bg-primary text-white">
+          Click Counter!
+        </div>
+        {cardBody}
+      </div>
+    );
+  }
+}
+
+export default Counter;
