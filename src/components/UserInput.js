532ce002b0ede0954c280adba6bea3d08a61724d
rasputinforever <erik.justin.portillo@gmail.com>
2021-03-27 18:33:03 -0700

    error for missing problem

--- src/components/UserInput.js
+++ src/components/UserInput.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Row, InputGroup, FormControl } from 'react-bootstrap';
 import API from "../utils/API";
+import ErrorMsg from "./inputError.js"
 
 class UserInput extends React.Component {
   constructor(props) {
@@ -9,11 +10,13 @@ class UserInput extends React.Component {
     this.state = {
         username: props.username,
         problem: props.problem,
-        response: false
+        response: false,
+        error: false
         }
   }
 
   handleInputChange = event => {
+
     const value = event.target.value;
     const name = event.target.name;
     
@@ -30,7 +33,7 @@ class UserInput extends React.Component {
 
         // split the problem sentence into an arr of words
         const wordArr = query.split(" ")
-        let foundWord = query;
+        let foundWord = '"' + query + '"';
 
         // do an API call to find the word-type, looking for nouns
 
@@ -39,7 +42,7 @@ class UserInput extends React.Component {
             API.search(wordArr[i])
             .then(res => {
               const wordType = res.data[0].fl
-              if (wordType.length > 2 && wordType === 'noun') {
+              if (wordArr[i].length > 2 && wordType === 'noun') {
                 
                 foundWord = wordArr[i]
               }
@@ -59,10 +62,19 @@ class UserInput extends React.Component {
   handleFormSubmit = event => {
     event.preventDefault();
 
+    if (this.state.problem === '') {
+      
+      // error message for empty problem
+      this.setState({ error: true })
+      return 
+      
+    }
+
     this.searchDictionary(this.state.problem)
     .then((res) => {
 
       this.setState({ 
+        error: false,
         problem: res,
         response: true, 
         }, function () {
@@ -99,6 +111,9 @@ class UserInput extends React.Component {
           </InputGroup>
 
           <button onClick={this.handleFormSubmit}>Submit</button>
+          
+          <Row>{this.state.error === true ? <ErrorMsg /> : <></>}</Row>
+
 
         </Row>
     );
---
+++ src/components/inputError.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class ErrorMsg extends React.Component {
+
+    render() {
+        return (
+            <h3>Please Enter a Problem!</h3>
+        )
+    }
+
+}
+
+export default ErrorMsg;
