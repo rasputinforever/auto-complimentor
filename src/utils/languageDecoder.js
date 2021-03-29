9e2c6f4dc56f3f765917974e9ac5ebae8758b46c
rasputinforever <erik.justin.portillo@gmail.com>
2021-03-28 22:56:54 -0700

    clean up

--- src/utils/languageDecoder.js
+++ src/utils/languageDecoder.js
@@ -12,24 +12,20 @@ async function wordAPI(wordArr) {
                         'word': word,
                         type: foundType.fl
                     }
-
                 })    
     }
-
     return wordArr
 }
 
 function processArr(wordObjArr) {
     // this is where we're going to create the "logic" to figure out what the subject of the sentence is. 
     let newStringArr = [];
-    
     let newString = ''
-
+    // we need index as the 'found noun' has to grab adjacent nouns and we're avoiding the for loop
     let index = 0
-
+    // nounstring is relevent deeper in the 'if' tree below. When hitting a noun, grab any adjacent nouns and adjectives.
     let nounString = [];
 
-    // some sentences may detect two sub-strings of "subjects".
     wordObjArr.forEach(word => {
         // replace I and MY with "Your"
         // definite article = 'the', keep that
@@ -41,7 +37,6 @@ function processArr(wordObjArr) {
             newStringArr.push("your")
         } else if (nounString.length === 0 && word.word.toUpperCase() !== 'I' && word.word.toUpperCase() !== 'MY' && word.type === 'noun') {
             
-
             if (wordObjArr[index - 1] && wordObjArr[index - 1].word.toUpperCase() !== 'MY' && wordObjArr[index - 1].type === 'adjective') {
                 nounString.push(wordObjArr[index - 1].word)
             }
@@ -55,7 +50,7 @@ function processArr(wordObjArr) {
             }
 
             newStringArr.push(nounString.join(" "))
-            
+    
             newString = newStringArr.join(" ")
 
         }
@@ -64,27 +59,24 @@ function processArr(wordObjArr) {
 
     })
 
-
-    // fail safe here, join with quotes on the word 
+    // fail safe here, join with quotes on the word, simulating that the mentor isn't sure what the hell you're talking about.
     if (!newString) {
-
         return false
-
     }
 
-    return newString
-
+    // will uppercase if necessary. Apologies to any proper nouns out there.
+    return newString.toLowerCase();
 }
 
-
+// this cranks through the statement and returns a truncated "subject" of the user's input to inject into the mentor-template
 function procStatement(string) {
     return new Promise(resolve => {
         const wordArr = string.split(" ")
 
-
         wordAPI(wordArr).then((res) => {
 
             let newString = processArr(res)
+            // will receive a false res if the user input doesn't contain any detected nouns or terrible spellings
             if (!newString) {
                 newString = '"' + string + '"'
             }
